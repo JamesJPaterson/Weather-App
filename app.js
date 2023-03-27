eae826d75abe8f3845f826456c0c62c7ce4681c2
James Paterson <james@mastudemb0jq6l4.home>
2023-03-27 17:35:57 +0100

    Comments added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const apiKey = "89ad92383ac5dd73e1f096eb9cbf58e3";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 function getWeather(city) {
+    // Am adding a fetch call to the OpenWeathermap API and returning the info which is then logged to the console as a JSON object 
     const url = `${apiUrl}?q=${city}&appid=${apiKey}&units=metric`;
   
     fetch(url)
@@ -11,4 +12,6 @@ function getWeather(city) {
         // TODO: Display weather information on the page
       })
       .catch(error => console.error(error));
-  }
+}
+
+
